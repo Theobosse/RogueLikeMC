35ee1f34e55aeb1dcb341016d4d00b0bcaf4da87
Theobosse <60153892+Theobosse@users.noreply.github.com>
2022-05-06 20:44:43 +0200

    first push

---
+++ src/main/java/fr/theobosse/roguelike/RogueLike.java
@@ -0,0 +1,11 @@
+package fr.theobosse.roguelike;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RogueLike extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("Test");
+    }
+}
