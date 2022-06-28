263bb72cb9bf792c09fc9f528bc5907d47dd99aa
Theobosse <60153892+Theobosse@users.noreply.github.com>
2022-06-28 14:56:23 +0200

    armor stand debug

--- src/main/java/fr/theobosse/roguelike/events/EnemiesEvent.java
+++ src/main/java/fr/theobosse/roguelike/events/EnemiesEvent.java
@@ -26,7 +26,8 @@ public class EnemiesEvent implements Listener {
     public void damaged(EntityDamageEvent event) {
         double damage = event.getFinalDamage();
         Entity entity = event.getEntity();
-        World world = entity.getWorld()
+        World world = entity.getWorld();
+
         NamespacedKey key = new NamespacedKey(RogueLike.instance, "life");
         PersistentDataContainer data = entity.getPersistentDataContainer();
         Location loc = entity.getLocation();
@@ -40,28 +41,25 @@ public class EnemiesEvent implements Listener {
             event.setDamage(0);
 
             // Summon Armor Stand with damage quantity
-            Entity armorStand;
-            armorStand = world.spawnEntity(loc, EntityType.ARMOR_STAND);
+            Random rnd = new Random();
+            ArmorStand armorStand = (ArmorStand) world.spawnEntity(loc.add(rnd.nextInt(3) - 1, rnd.nextInt(2), rnd.nextInt(3)-1), EntityType.ARMOR_STAND);
             armorStand.setCustomName(String.valueOf(damage));
+            armorStand.setCustomNameVisible(true);
+            armorStand.setGravity(false);
+            armorStand.setVisible(false);
 
             // Kill
             if (life - damage <= 0) {
                 entity.remove();
 
                 ((ExperienceOrb) entity.getWorld().spawn(loc, EntityType.EXPERIENCE_ORB.getEntityClass())).setExperience(section.getInt("xp"));
-                entity.getWorld().spawnParticle(Particle.SOUL, loc.add(0, 1, 0), 10, 0.5, 0.5, 0.5, 1, 0, true);
-                entity.getWorld().spawnParticle(Particle.SOUL_FIRE_FLAME, loc.add(0, 1, 0), 10, 0.5, 0.5, 0.5, 1, 0, true);
-                Random rnd = new Random();
-                ArmorStand armowo = (ArmorStand) loc.getWorld().spawnEntity(loc.add(rnd.nextInt(3)-1,rnd.nextInt(2),rnd.nextInt(3)-1), EntityType.ARMOR_STAND);
-                armowo.setCustomName(String.valueOf(damage));
-                armowo.setVisible(false);
-                armowo.setCustomNameVisible(true);
+                entity.getWorld().spawnParticle(Particle.SOUL, loc.add(0, 1, 0), 10, 0.5, 0.5, 0.5, 1);
+                entity.getWorld().spawnParticle(Particle.SOUL_FIRE_FLAME, loc.add(0, 1, 0), 10, 0.5, 0.5, 0.5, 1);
 
                 if (section.contains("drop")){
                     for(String item : section.getConfigurationSection("drop").getKeys(false)){
                         if (section.contains("drop." + item + ".percent-drop")){
                             int percent_drop = section.getInt("drop." + item + ".percent-drop");
-                            Random rnd = new Random();
                             if (rnd.nextInt(100) < percent_drop)
                                 entity.getWorld().dropItem(loc, new ItemBuilder(section.getConfigurationSection("drop." + item)).getItem());
                         }else {
@@ -71,7 +69,5 @@ public class EnemiesEvent implements Listener {
                 }
             }
         }
-
     }
-
 }
