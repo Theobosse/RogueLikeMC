3b77216434a877fefc23b04fd90bd5311c5e0c9e
Theobosse <60153892+Theobosse@users.noreply.github.com>
2022-06-28 13:38:59 +0200

    drop

--- src/main/java/fr/theobosse/roguelike/events/EnemiesEvent.java
+++ src/main/java/fr/theobosse/roguelike/events/EnemiesEvent.java
@@ -2,6 +2,7 @@ package fr.theobosse.roguelike.events;
 
 import fr.theobosse.roguelike.RogueLike;
 import fr.theobosse.roguelike.tools.Configs;
+import fr.theobosse.roguelike.tools.ItemBuilder;
 import org.bukkit.Location;
 import org.bukkit.NamespacedKey;
 import org.bukkit.Particle;
@@ -9,10 +10,12 @@ import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.ExperienceOrb;
+import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 
@@ -40,6 +43,12 @@ public class EnemiesEvent implements Listener {
                 ((ExperienceOrb) entity.getWorld().spawn(loc, EntityType.EXPERIENCE_ORB.getEntityClass())).setExperience(section.getInt("xp"));
                 entity.getWorld().spawnParticle(Particle.SOUL, loc.add(0, 1, 0), 10, 0.5, 0.5, 0.5, 1, 0, true);
                 entity.getWorld().spawnParticle(Particle.SOUL_FIRE_FLAME, loc.add(0, 1, 0), 10, 0.5, 0.5, 0.5, 1, 0, true);
+
+                if (section.contains("drop")){
+                    for(String item : section.getConfigurationSection("drop").getKeys(false)){
+                        entity.getWorld().dropItem(loc, new ItemBuilder(section.getConfigurationSection("drop." + item)).getItem());
+                    }
+                }
             }
         }
 
