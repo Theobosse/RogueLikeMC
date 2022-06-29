4f0df6e228e0b446ef3df586876b8052711fa18d
Theobosse <60153892+Theobosse@users.noreply.github.com>
2022-06-29 19:21:42 +0200

    Ammo system

--- src/main/java/fr/theobosse/roguelike/events/AmmoEvent.java
+++ src/main/java/fr/theobosse/roguelike/events/AmmoEvent.java
@@ -1,8 +1,7 @@
 package fr.theobosse.roguelike.events;
 
 import fr.theobosse.roguelike.RogueLike;
-import fr.theobosse.roguelike.game.User;
-import fr.theobosse.roguelike.game.Weapon;
+import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.Sound;
 import org.bukkit.SoundCategory;
@@ -11,14 +10,15 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 
-public class DurabilityEvent implements Listener {
+public class AmmoEvent implements Listener {
 
-    @EventHandler(priority = EventPriority.LOWEST)
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void hitEvent(EntityDamageByEntityEvent event) {
         if (!(event.getDamager() instanceof Player)) return;
         Player player = (Player) event.getDamager();
@@ -48,6 +48,40 @@ public class DurabilityEvent implements Listener {
             }
         }
         is.setItemMeta(im);
+    }
+
+    @EventHandler
+    public void onInvClick(InventoryClickEvent event) {
+        if (!event.getClick().isRightClick()) return;
+        if (!(event.getWhoClicked() instanceof Player)) return;
+        Player player = (Player) event.getWhoClicked();
+        ItemStack wp = event.getCurrentItem();
+        ItemStack ammo = event.getCursor();
+
+        if (ammo == null || wp == null || ammo.getItemMeta() == null || wp.getItemMeta() == null) return;
+        ItemMeta ammoMeta = ammo.getItemMeta();
+        ItemMeta wpMeta = wp.getItemMeta();
+
+        PersistentDataContainer ammoData = ammoMeta.getPersistentDataContainer();
+        PersistentDataContainer wpData = wpMeta.getPersistentDataContainer();
+        NamespacedKey ammoKey = new NamespacedKey(RogueLike.instance, "amount");
+        NamespacedKey wpKey = new NamespacedKey(RogueLike.instance, "ammo");
+        NamespacedKey mwpKey = new NamespacedKey(RogueLike.instance, "maxAmmo");
+
+        if (ammoData.has(ammoKey, PersistentDataType.INTEGER) && wpData.has(wpKey, PersistentDataType.INTEGER) && wpData.has(mwpKey, PersistentDataType.INTEGER)) {
+            event.setCancelled(true);
+            int weaponAmmo = wpData.get(wpKey, PersistentDataType.INTEGER);
+            int maxWeaponAmmo = wpData.get(mwpKey, PersistentDataType.INTEGER);
+            int ammoAmount = ammoData.get(ammoKey, PersistentDataType.INTEGER);
+            int newAmmo = Math.min(maxWeaponAmmo, weaponAmmo + ammoAmount);
+
+            wpData.set(wpKey, PersistentDataType.INTEGER, newAmmo);
+            wpMeta.setDisplayName(wpMeta.getDisplayName().split(" §6§l>> ")[0] + " §6§l>> §e" + newAmmo + " §6/ §e" + maxWeaponAmmo);
+            ammo.setItemMeta(ammoMeta);
+            wp.setItemMeta(wpMeta);
+
+            player.playSound(player, Sound.ENTITY_PLAYER_LEVELUP, 6, 3);
+        }
     }
 
 }
